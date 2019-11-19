ec3f9788ad79b65c2945c3a88517c930e55be68d
hcymysql <chunyang_he@139.com>
2019-11-19 15:21:42 +0800

    update

---
+++ mail/mail.php
@@ -0,0 +1,22 @@
+<?php
+//header("Content-type:text/html;charset=utf-8;");
+
+class mail{
+	private $send_mail_to_list;
+	private $alarm_subject;
+	private $alarm_info;
+
+	function __construct($send_mail_to_list,$alarm_subject,$alarm_info){
+		$this->send_mail_to_list = $send_mail_to_list;
+		$this->alarm_subject = $alarm_subject;
+		$this->alarm_info = $alarm_info;
+	}
+	
+	function execCommand(){
+	echo $this->alarm_subject."\n";
+	echo $this->alarm_info."\n";
+	system("./mail/sendEmail -f chunyang_he@139.com -t '{$this->send_mail_to_list}' -s smtp.139.com:25 -u '{$this->alarm_subject}' -o message-charset=utf8 -o message-content-type=html -m '报警信息：<br><font color='#FF0000'>{$this->alarm_info}</font>' -xu chunyang_he@139.com -xp 'WDhcy159753@' -o tls=no");
+	}
+}
+
+?>

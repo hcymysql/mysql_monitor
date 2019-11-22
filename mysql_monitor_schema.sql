54252f1fa269ad32eb4ea7b19f5775c7cc5607bd
hcymysql <chunyang_he@139.com>
2019-11-22 15:59:47 +0800

    Add files via upload

--- mysql_monitor_schema.sql
+++ mysql_monitor_schema.sql
@@ -1,35 +1,25 @@
--- MySQL dump 10.13  Distrib 8.0.18, for linux-glibc2.12 (x86_64)
---
--- Host: localhost    Database: sql_db
--- ------------------------------------------------------
--- Server version	8.0.18
+/*
+SQLyog Professional v10.42 
+MySQL - 8.0.18 : Database - sql_db
+*********************************************************************
+*/
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Current Database: `sql_db`
---
-
-CREATE DATABASE /*!32312 IF NOT EXISTS*/ `sql_db` /*!40100 DEFAULT CHARACTER SET utf8 */ /*!80016 DEFAULT ENCRYPTION='N' */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`sql_db` /*!40100 DEFAULT CHARACTER SET utf8 */ /*!80016 DEFAULT ENCRYPTION='N' */;
 
 USE `sql_db`;
 
---
--- Table structure for table `mysql_repl_status`
---
+/*Table structure for table `mysql_repl_status` */
 
 DROP TABLE IF EXISTS `mysql_repl_status`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+
 CREATE TABLE `mysql_repl_status` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `server_id` int(11) DEFAULT NULL,
@@ -52,17 +42,14 @@ CREATE TABLE `mysql_repl_status` (
   `Last_IO_Error` varchar(500) DEFAULT NULL,
   `Last_SQL_Error` varchar(500) DEFAULT NULL,
   `create_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  PRIMARY KEY (`id`),
+  KEY `IX_h_d_p_c` (`host`,`dbname`,`port`,`create_time`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
---
--- Table structure for table `mysql_status`
---
+/*Table structure for table `mysql_status` */
 
 DROP TABLE IF EXISTS `mysql_status`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+
 CREATE TABLE `mysql_status` (
   `id` bigint(10) unsigned NOT NULL AUTO_INCREMENT,
   `host` varchar(30) DEFAULT NULL,
@@ -79,17 +66,14 @@ CREATE TABLE `mysql_status` (
   `runtime` int(11) DEFAULT NULL,
   `db_version` varchar(100) DEFAULT NULL,
   `create_time` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=208 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  PRIMARY KEY (`id`),
+  KEY `IX_h_d_p_c` (`host`,`dbname`,`port`,`create_time`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
---
--- Table structure for table `mysql_status_history`
---
+/*Table structure for table `mysql_status_history` */
 
 DROP TABLE IF EXISTS `mysql_status_history`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+
 CREATE TABLE `mysql_status_history` (
   `id` bigint(10) unsigned NOT NULL AUTO_INCREMENT,
   `host` varchar(30) DEFAULT NULL,
@@ -108,17 +92,13 @@ CREATE TABLE `mysql_status_history` (
   `create_time` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `create_time` (`create_time`),
-  KEY `IX_h_d_p` (`host`,`dbname`,`port`)
-) ENGINE=InnoDB AUTO_INCREMENT=441 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  KEY `IX_h_d_p_c` (`host`,`dbname`,`port`,`create_time`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
---
--- Table structure for table `mysql_status_info`
---
+/*Table structure for table `mysql_status_info` */
 
 DROP TABLE IF EXISTS `mysql_status_info`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+
 CREATE TABLE `mysql_status_info` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `ip` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '输入被监控MySQL的IP地址',
@@ -133,17 +113,13 @@ CREATE TABLE `mysql_status_info` (
   `send_weixin_to_list` varchar(100) DEFAULT NULL COMMENT '微信公众号',
   `alarm_threads_running` tinyint(4) DEFAULT NULL COMMENT '记录活动连接数告警信息，1为已记录',
   `threshold_alarm_threads_running` tinyint(4) DEFAULT NULL COMMENT '设置连接数阀值',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=40 DEFAULT CHARSET=utf8 COMMENT='监控信息表';
-/*!40101 SET character_set_client = @saved_cs_client */;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+  `alarm_repl_status` tinyint(4) DEFAULT NULL COMMENT '记录主从复制告警信息，1为记录主从状态，3为记录主从延迟状态',
+  `threshold_warning_repl_delay` tinyint(4) DEFAULT NULL COMMENT '设置主从复制延迟阀值',
+  PRIMARY KEY (`id`),
+  KEY `IX_i_d_p` (`ip`,`dbname`,`port`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COMMENT='监控信息表';
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
 /*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2019-11-20 16:28:54

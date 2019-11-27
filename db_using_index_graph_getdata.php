2c471d33fac83e5c202b612587cbcfea57aacaef
hcymysql <chunyang_he@139.com>
2019-11-27 17:51:43 +0800

    Add files via upload

---
+++ db_using_index_graph_getdata.php
@@ -0,0 +1,53 @@
+<?php
+
+function index($arr1,$arr2,$arr3){
+    ini_set('date.timezone','Asia/Shanghai');
+    /*
+    $ip = $_GET['ip'];
+    $dbname = $_GET['dbname'];
+    $port = $_GET['port'];
+    */
+   
+    $ip = $arr1;
+    $dbname = $arr2;
+    $port = $arr3;
+
+    require 'conn.php';
+    //$get_info="select create_time,qps_select,qps_insert,qps_update,qps_delete from mysql_status_history where host='${ip}' and dbname='${dbname}' and port=${port} and create_time >=DATE_FORMAT(now(),'%Y-%m-%d')";
+    $get_info="select create_time,Handler_read_key,Handler_read_rnd_next from mysql_status_history where host='${ip}' and dbname='${dbname}' and port=${port} and  create_time >=DATE_FORMAT(SUBDATE(now(),interval 1 day),'%Y-%m-%d')"; 
+    $result1 = mysqli_query($con,$get_info);
+	//echo $get_info;
+
+  $array=array();
+  class UsingIndex{
+    public $create_time;
+    public $Handler_read_key;
+    public $Handler_read_rnd_next;
+  }
+  while($row = mysqli_fetch_array($result1,MYSQL_ASSOC)){
+    $cons=new UsingIndex();
+    $cons->create_time = $row['create_time'];
+    $cons->Handler_read_key = $row['Handler_read_key'];
+    $cons->Handler_read_rnd_next = $row['Handler_read_rnd_next'];
+    $array[]=$cons;
+  }
+  $top_data=json_encode($array);
+  // echo "{".'"user"'.":".$data."}";
+ echo $top_data;
+}
+
+/*$fn = isset($_GET['fn']) ? $_GET['fn'] : 'main';
+if (function_exists($fn)) {
+  call_user_func($fn);
+}
+*/
+
+    $ip = $_GET['ip'];
+    $dbname = $_GET['dbname'];
+    $port = $_GET['port'];
+
+index($ip,$dbname,$port);
+
+
+?>
+

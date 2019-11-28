8db18a5f2a7d324a98688b9be6467bf589a244cf
hcymysql <chunyang_he@139.com>
2019-11-28 16:15:08 +0800

    Add files via upload

--- weixin/wechat.py
+++ weixin/wechat.py
@@ -1,59 +1,74 @@
-#!/usr/bin/python
+#!/usr/bin/python2.7
 #_*_coding:utf-8 _*_
+#auther:火星小刘
 
- 
-import urllib,urllib2
-import json
-import sys
-import simplejson
+import requests,sys,json
+import urllib3
+urllib3.disable_warnings()
 
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
+def GetTokenFromServer(Corpid,Secret):
+    Url = "https://qyapi.weixin.qq.com/cgi-bin/gettoken"
+    Data = {
+        "corpid":Corpid,
+        "corpsecret":Secret
+    }
+    r = requests.get(url=Url,params=Data,verify=False)
+    print(r.json())
+    if r.json()['errcode'] != 0:
+        return False
+    else:
+        Token = r.json()['access_token']
+        file = open('/tmp/zabbix_wechat_config.json', 'w')
+        file.write(r.text)
+        file.close()
+        return Token
 
-def gettoken(corpid,corpsecret):
-    gettoken_url = 'https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid=' + corpid + '&corpsecret=' + corpsecret
-    print  gettoken_url
+def SendMessage(User,Agentid,Subject,Content):
     try:
-        token_file = urllib2.urlopen(gettoken_url)
-    except urllib2.HTTPError as e:
-        print e.code
-        print e.read().decode("utf8")
-        sys.exit()
-    token_data = token_file.read().decode('utf-8')
-    token_json = json.loads(token_data)
-    token_json.keys()
-    token = token_json['access_token']
-    return token
- 
- 
- 
-def senddata(access_token,user,subject,content):
- 
-    send_url = 'https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token=' + access_token
-    send_values = {
-        "touser":user,    #企业号中的用户帐号，在zabbix用户Media中配置，如果配置不正常，将按部门发送。
-        "toparty":"1",    #企业号中的部门id。
-        "msgtype":"text", #消息类型。
-        "agentid":"1",    #企业号中的应用id。
-        "text":{
-            "content":subject + '\n' + content
-           },
-        "safe":"0"
-        }
-#    send_data = json.dumps(send_values, ensure_ascii=False)
-    send_data = simplejson.dumps(send_values, ensure_ascii=False).encode('utf-8')
-    send_request = urllib2.Request(send_url, send_data)
-    response = json.loads(urllib2.urlopen(send_request).read())
-    print str(response)
- 
- 
+        file = open('/tmp/zabbix_wechat_config.json', 'r')
+        Token = json.load(file)['access_token']
+        file.close()
+    except:
+        Token = GetTokenFromServer(Corpid, Secret)
+
+    n = 0
+    Url = "https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token=%s" % Token
+    Data = {
+        "touser": User,                                 # 企业号中的用户帐号，在zabbix用户Media中配置，如果配置不正常，将按部门发送。
+        #"totag": Tagid,                                # 企业号中的标签id，群发使用（推荐）
+        #"toparty": Partyid,                             # 企业号中的部门id，群发时使用。
+        "msgtype": "text",                              # 消息类型。
+        "agentid": Agentid,                             # 企业号中的应用id。
+        "text": {
+            "content": Subject + '\n' + Content
+        },
+        "safe": "0"
+    }
+    r = requests.post(url=Url,data=json.dumps(Data),verify=False)
+    while r.json()['errcode'] != 0 and n < 4:
+        n+=1
+        Token = GetTokenFromServer(Corpid, Secret)
+        if Token:
+            Url = "https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token=%s" % Token
+            r = requests.post(url=Url,data=json.dumps(Data),verify=False)
+            print(r.json())
+
+    return r.json()
+
+
 if __name__ == '__main__':
-    user = str(sys.argv[1])     #zabbix传过来的第一个参数
-    subject = str(sys.argv[2])  #zabbix传过来的第二个参数
-    content = str(sys.argv[3])  #zabbix传过来的第三个参数
-    
-    corpid =  'wxxxxxxxxxxxxxxxxx'   #CorpID是企业号的标识，修改成你自己的
-    corpsecret =  '_wmxxxxxxxxxxxxxxxxxxxxxxxxx' #corpsecretSecret是管理组凭证密钥，修改成你自己的
-    accesstoken = gettoken(corpid,corpsecret)
-    senddata(accesstoken,user,subject,content)
+    User = sys.argv[1]                                                                # zabbix传过来的第一个参数
+    Subject = str(sys.argv[2])                                                             # zabbix传过来的第二个参数
+    Content = str(sys.argv[3])                                                             # zabbix传过来的第三个参数
+
+    Corpid = "wxaf"                                                                     # CorpID是企业号的标识
+    Secret = "aKDdCRT76"                                                                # Secret是管理组凭证密钥
+    #Tagid = "1"                                                                        # 通讯录标签ID
+    Agentid = "1000001"                                                                 # 应用ID
+    #Partyid = "1"                                                                      # 部门ID
+
+    Status = SendMessage(User,Agentid,Subject,Content)
+    print Status

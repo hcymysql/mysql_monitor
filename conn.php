a38630282d3eb492669b4b477cd73574b4a8a07f
hcymysql <chunyang_he@139.com>
2019-12-23 17:44:22 +0800

    Update conn.php

--- conn.php
+++ conn.php
@@ -1,6 +1,6 @@
 <?php         
 //https://github.com/hcymysql/mysql_monitor
 
-     $con = mysqli_connect("10.10.159.31","admin","hechunyang","sql_db","3306") or die("数据库链接错误".mysql_error());
+     $con = mysqli_connect("10.10.159.31","admin","hechunyang","sql_db","3306") or die("数据库链接错误".mysqli_error($con));
      mysqli_query($con,"set names utf8");  
 ?> 

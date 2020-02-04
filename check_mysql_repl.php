db994d913896a2417d86c304b231b47f17e620b9
hcymysql <chunyang_he@139.com>
2020-02-04 22:59:04 +0800

    修正主从信息状态错误
    修正主从信息状态错误

--- check_mysql_repl.php
+++ check_mysql_repl.php
@@ -4,7 +4,7 @@ header("Content-type:text/html;charset=utf-8;");
 ini_set('date.timezone','Asia/Shanghai');
 
 require 'conn.php';
-include 'convert_array.php';
+include 'filter_array.php';
 include 'mail/mail.php';
 include 'weixin/weixin.php';
 	 
@@ -64,43 +64,43 @@ do {
         $processed++;
     }
 } while ($processed < count($all_links));
-	//echo "---------------------------"."\n";
-        //print_r(convertarray($re)); //调试
-	$re=convertarray($re); //将二维数组转换为一维数组，方便取出数据库状态值
-	
+	echo "---------------------------"."\n";
+	//print_r(array_values(array_filter(filter_array($re)))); //调试
+	$re=array_values(array_filter(filter_array($re)));
+
 	//1为Primary，0为Secondary
-	$role=!isset($re['Slave_IO_State'])?1:0;
-	$gtid=$re['Auto_Position']==1?'ON':'OFF';
+	$role=!isset($re[0]['Slave_IO_State'])?1:0;
+	$gtid=$re[0]['Auto_Position']==1?'ON':'OFF';
 	echo "角色是:".$role_status=$role==1?'Primary'.PHP_EOL:'Secondary'.PHP_EOL;
 	$is_live=isset($connect_error)?0:1;
 	//echo $is_live."\n";
-	$Last_IO_Error=preg_replace('/\'/', '', $re['Last_IO_Error']);
-	$Last_SQL_Error=preg_replace('/\'/', '', $re['Last_SQL_Error']);
+	$Last_IO_Error=preg_replace('/\'/', '', $re[0]['Last_IO_Error']);
+	$Last_SQL_Error=preg_replace('/\'/', '', $re[0]['Last_SQL_Error']);
 	
 	if($is_live==0){
 	    echo "$ip"."\n";
-            echo $connect_error."\n";
+        echo $connect_error."\n";
 	    unset($connect_error);
 	    $sql = "INSERT INTO mysql_repl_status(host,dbname,port,role,is_live) VALUES('{$ip}','{$dbname}','{$port}','{$role}','{$is_live}')";	
 	}  else {
 		
 		//同步状态监控
-	     if($re['Slave_IO_Running']=='No' || $re['Slave_SQL_Running']=='No' ){
-			 echo "【报错】主从同步复制Slave_IO_Running状态是：{$re['Slave_IO_Running']}; Slave_SQL_Running状态是：{$re['Slave_SQL_Running']}\n";
+	    if($re[0]['Slave_IO_Running']=='No' || $re[0]['Slave_SQL_Running']=='No' ){
+			 echo "【报错】主从同步复制Slave_IO_Running状态是：{$re[0]['Slave_IO_Running']}; Slave_SQL_Running状态是：{$re[0]['Slave_SQL_Running']}\n";
 	    //告警---------------------  
 	    if($send_mail==0 || empty($send_mail)){
 			echo "被监控主机：$ip  【{$dbname}库】【端口{$port}】关闭邮件监控报警。"."\n";
 	    } else {
 	    	$alarm_subject = "【报错】被监控主机：".$ip."  【{$dbname}库】【端口{$port}】主从同步复制异常 ".date("Y-m-d H:i:s");
-	    	$alarm_info = "被监控主机：".$ip."   【{$dbname}库】【端口{$port}】主从同步复制Slave_IO_Running状态是：{$re['Slave_IO_Running']}; Slave_SQL_Running状态是：{$re['Slave_SQL_Running']}";
+	    	$alarm_info = "被监控主机：".$ip."   【{$dbname}库】【端口{$port}】主从同步复制Slave_IO_Running状态是：{$re[0]['Slave_IO_Running']}; Slave_SQL_Running状态是：{$re[0]['Slave_SQL_Running']}";
 	    	$sendmail = new mail($send_mail_to_list,$alarm_subject,$alarm_info);
-                $sendmail->execCommand();
+            $sendmail->execCommand();
 	    }
 	    if($send_weixin==0 || empty($send_weixin)){
-		echo "被监控主机：$ip  【{$dbname}库】【端口{$port}】关闭微信监控报警。"."\n";
+			echo "被监控主机：$ip  【{$dbname}库】【端口{$port}】关闭微信监控报警。"."\n";
 	    } else {
 		$alarm_subject = "【报错】被监控主机：".$ip."  【{$dbname}库】【端口{$port}】主从同步复制异常 ".date("Y-m-d H:i:s");
-		$alarm_info = "被监控主机：".$ip."   【{$dbname}库】【端口{$port}】主从同步复制Slave_IO_Running状态是：{$re['Slave_IO_Running']}; Slave_SQL_Running状态是：{$re['Slave_SQL_Running']}";
+		$alarm_info = "被监控主机：".$ip."   【{$dbname}库】【端口{$port}】主从同步复制Slave_IO_Running状态是：{$re[0]['Slave_IO_Running']}; Slave_SQL_Running状态是：{$re[0]['Slave_SQL_Running']}";
 		$sendweixin = new weixin($send_weixin_to_list,$alarm_subject,$alarm_info);
 		$sendweixin->execCommand();
 	    }
@@ -117,13 +117,13 @@ do {
                 echo "被监控主机：$ip  【{$dbname}库】【端口{$port}】关闭微信监控报警。"."\n";
             }
 	      if(($send_mail==1 || $send_weixin==1)){
-		    $recover_repl_status_sql = "SELECT alarm_repl_status FROM mysql_status_info WHERE IP='{$ip}' AND dbname='{$dbname}' AND PORT='{$port}' ";
+				$recover_repl_status_sql = "SELECT alarm_repl_status FROM mysql_status_info WHERE IP='{$ip}' AND dbname='{$dbname}' AND PORT='{$port}' ";
 	    	    $recover_repl_status = mysqli_query($con, $recover_repl_status_sql);
 	    	    $recover_repl_status_row = mysqli_fetch_assoc($recover_repl_status);
 	      }			
 	    if(!empty($recover_repl_status_row['alarm_repl_status']) && $recover_repl_status_row['alarm_repl_status'] == 1 ){
 		    $recover_subject = "【恢复】被监控主机：".$ip."  【{$dbname}库】【端口{$port}】主从同步复制已恢复 ".date("Y-m-d H:i:s");
-		    $recover_info = "主从同步复制Slave_IO_Running状态是：{$re['Slave_IO_Running']}; Slave_SQL_Running状态是：{$re['Slave_SQL_Running']}";
+		    $recover_info = "主从同步复制Slave_IO_Running状态是：{$re[0]['Slave_IO_Running']}; Slave_SQL_Running状态是：{$re[0]['Slave_SQL_Running']}";
 		    if($send_mail==1 ){
 			  $sendmail = new mail($send_mail_to_list,$recover_subject,$recover_info);
 			  $sendmail->execCommand();
@@ -140,22 +140,22 @@ do {
 	   
 	   
 	   //同步延迟监控
-	   if(!empty($threshold_warning_repl_delay) && $re['Seconds_Behind_Master']>=$threshold_warning_repl_delay){
-	   echo "【告警】 【{$dbname}库】主从同步延迟{$re['Seconds_Behind_Master']}秒\n";
+	    if(!empty($threshold_warning_repl_delay) && $re[0]['Seconds_Behind_Master']>=$threshold_warning_repl_delay){
+			echo "【告警】 【{$dbname}库】【端口{$port}】主从同步延迟{$re[0]['Seconds_Behind_Master']}秒\n";
 	    //告警---------------------  
 	    if($send_mail==0 || empty($send_mail)){
 			echo "被监控主机：$ip  【{$dbname}库】【端口{$port}】关闭邮件监控报警。"."\n";
 	    } else {
 	    	$alarm_subject = "【告警】被监控主机：".$ip."  【{$dbname}库】【端口{$port}】主从同步延迟 ".date("Y-m-d H:i:s");
-	    	$alarm_info = "被监控主机：".$ip."   【{$dbname}库】【端口{$port}】主从同步延迟{$re['Seconds_Behind_Master']}秒";
+	    	$alarm_info = "被监控主机：".$ip."   【{$dbname}库】【端口{$port}】主从同步延迟{$re[0]['Seconds_Behind_Master']}秒";
 	    	$sendmail = new mail($send_mail_to_list,$alarm_subject,$alarm_info);
-                $sendmail->execCommand();
+            $sendmail->execCommand();
 	    }
 	    if($send_weixin==0 || empty($send_weixin)){
 		echo "被监控主机：$ip  【{$dbname}库】【端口{$port}】关闭微信监控报警。"."\n";
 	    } else {
 		$alarm_subject = "【告警】被监控主机：".$ip."  【{$dbname}库】【端口{$port}】主从同步延迟 ".date("Y-m-d H:i:s");
-		$alarm_info = "被监控主机：".$ip."   【{$dbname}库】【端口{$port}】主从同步延迟{$re['Seconds_Behind_Master']}秒";
+		$alarm_info = "被监控主机：".$ip."   【{$dbname}库】【端口{$port}】主从同步延迟{$re[0]['Seconds_Behind_Master']}秒";
 		$sendweixin = new weixin($send_weixin_to_list,$alarm_subject,$alarm_info);
 		$sendweixin->execCommand();
 	    }
@@ -163,7 +163,7 @@ do {
 	          $update_slave_status = "UPDATE mysql_status_info SET alarm_repl_status = 3 WHERE IP='{$ip}' AND dbname='{$dbname}' AND PORT='{$port}'";
 	          mysqli_query($con, $update_slave_status);
 	    } 
-	   } 
+	    } 
 	   else {
 	    //恢复---------------------
             if($send_mail==0 || empty($send_mail)){
@@ -172,14 +172,14 @@ do {
             if($send_weixin==0 || empty($send_weixin)){
                 echo "被监控主机：$ip  【{$dbname}库】【端口{$port}】关闭微信监控报警。"."\n";
             }
-	      if(($send_mail==1 || $send_weixin==1)){
+	        if(($send_mail==1 || $send_weixin==1)){
 		    $recover_repl_status_sql = "SELECT alarm_repl_status FROM mysql_status_info WHERE IP='{$ip}' AND dbname='{$dbname}' AND PORT='{$port}' ";
 	    	    $recover_repl_status = mysqli_query($con, $recover_repl_status_sql);
 	    	    $recover_repl_status_row = mysqli_fetch_assoc($recover_repl_status);
-	      }			
+	        }			
 	    if(!empty($recover_repl_status_row['alarm_repl_status']) && $recover_repl_status_row['alarm_repl_status'] == 3 ){
-		    $recover_subject = "【恢复】被监控主机：".$ip."  【{$dbname}库】【端口{$port}】主从同步延迟已恢复 ".date("Y-m-d H:i:s");
-		    $recover_info = "主从同步延迟{$re['Seconds_Behind_Master']}秒";
+		    $recover_subject = "【恢复】被监控主机：".$ip."  【{$dbname}库】主从同步延迟已恢复 ".date("Y-m-d H:i:s");
+		    $recover_info = "主从同步延迟{$re[0]['Seconds_Behind_Master']}秒";
 		    if($send_mail==1 ){
 			  $sendmail = new mail($send_mail_to_list,$recover_subject,$recover_info);
 			  $sendmail->execCommand();
@@ -195,17 +195,18 @@ do {
 	   } // end else Seconds_Behind_Master
 	} // end else is_live
 //---------------------------------------------------------------------------
-	     $sql = "INSERT INTO mysql_repl_status(server_id,host,dbname,port,role,is_live,read_only,gtid_mode,Master_Host,Master_Port,Slave_IO_Running,Slave_SQL_Running,Seconds_Behind_Master,Master_Log_File,Relay_Master_Log_File,Read_Master_Log_Pos,Exec_Master_Log_Pos,Last_IO_Error,Last_SQL_Error,create_time) values('{$re['server_id']}','{$ip}','{$dbname}','{$port}','{$role}','{$is_live}','{$re['read_only']}','{$gtid}','{$re['Master_Host']}','{$re['Master_Port']}','{$re['Slave_IO_Running']}','{$re['Slave_SQL_Running']}','{$re['Seconds_Behind_Master']}','{$re['Master_Log_File']}','{$re['Relay_Master_Log_File']}','{$re['Read_Master_Log_Pos']}','{$re['Exec_Master_Log_Pos']}','{$Last_IO_Error}','{$Last_SQL_Error}',now())";
+	     $sql = "INSERT INTO mysql_repl_status(server_id,host,dbname,port,role,is_live,read_only,gtid_mode,Master_Host,Master_Port,Slave_IO_Running,Slave_SQL_Running,Seconds_Behind_Master,Master_Log_File,Relay_Master_Log_File,Read_Master_Log_Pos,Exec_Master_Log_Pos,Last_IO_Error,Last_SQL_Error,create_time) values('" .end($re)['server_id']. "','{$ip}','{$dbname}','{$port}','{$role}','{$is_live}','" .end($re)['read_only']. "','{$gtid}','{$re[0]['Master_Host']}','{$re[0]['Master_Port']}','{$re[0]['Slave_IO_Running']}','{$re[0]['Slave_SQL_Running']}','{$re[0]['Seconds_Behind_Master']}','{$re[0]['Master_Log_File']}','{$re[0]['Relay_Master_Log_File']}','{$re[0]['Read_Master_Log_Pos']}','{$re[0]['Exec_Master_Log_Pos']}','{$Last_IO_Error}','{$Last_SQL_Error}',now())";
 		
         if (mysqli_query($con, $sql)) {
             echo "{$ip}:'{$dbname}'新记录插入成功\n";
-	      echo "---------------------------\n\n";
-		mysqli_query($con,"delete from mysql_repl_status where host='{$ip}' and dbname='{$dbname}' and port='{$port}' and create_time<DATE_SUB(now(),interval 10 second)");
+	        echo "---------------------------\n\n";
+			mysqli_query($con,"delete from mysql_repl_status where host='{$ip}' and dbname='{$dbname}' and port='{$port}' and create_time<DATE_SUB(now(),interval 100 second)");
         } else {
-	      echo "{$ip}:'{$dbname}'新记录插入失败\n";
+			echo "{$ip}:'{$dbname}'新记录插入失败\n";
             echo "Error: " . $sql . "\n" . mysqli_error($con);
         }
-	
+
+
 //清空每个IP数据库状态值	
 array_splice($re, 0, count($re));	
 array_splice($r, 0, count($r));

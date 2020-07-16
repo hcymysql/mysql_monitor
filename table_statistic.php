e4039e80ce4fa389ffa0433b86dc36c466d5547d
hcymysql <chunyang_he@139.com>
2020-07-16 15:01:05 +0800

    增加主键自增键值监控
    增加主键自增键值监控

--- table_statistic.php
+++ table_statistic.php
@@ -44,7 +44,7 @@ a:visited { text-decoration: none;color: green}
 
 <body>
 
-<div class="col-md-10">
+<div class="col-md-05">
 <div class="card">
 <div class="card-body">
 <div class="table-responsive">
@@ -66,6 +66,10 @@ a:visited { text-decoration: none;color: green}
 <th>数据大小(GB)</th>
 <th>索引大小(GB)</th>
 <th>总计(GB)</th>
+<th>主键自增字段</th>
+<th>主键字段属性</th>
+<th>主键自增当前值</th>
+<th>主键自增值剩余</th>
 </tr>
 </thead>
 <tbody>
@@ -74,17 +78,45 @@ a:visited { text-decoration: none;color: green}
      $con_table_info = mysqli_connect($ip,$user,$pwd,$dbname,$port) or die("数据库链接错误". PHP_EOL .mysqli_connect_error());
 	mysqli_query($con_table_info,"set sql_mode=''");  
 	
-     $get_table_info ="SELECT TABLE_NAME,ENGINE,DATA_LENGTH/1024/1024/1024,INDEX_LENGTH/1024/1024/1024,SUM(DATA_LENGTH+INDEX_LENGTH)/1024/1024/1024 AS TOTAL_LENGTH FROM information_schema.TABLES WHERE TABLE_SCHEMA='{$dbname}' GROUP BY TABLE_NAME ORDER BY TOTAL_LENGTH DESC";
+     #$get_table_info ="SELECT TABLE_NAME,ENGINE,DATA_LENGTH/1024/1024/1024,INDEX_LENGTH/1024/1024/1024,SUM(DATA_LENGTH+INDEX_LENGTH)/1024/1024/1024 AS TOTAL_LENGTH FROM information_schema.TABLES WHERE TABLE_SCHEMA='{$dbname}' GROUP BY TABLE_NAME ORDER BY TOTAL_LENGTH DESC";
+	 
+	$get_table_info ="SELECT t.TABLE_NAME as TABLE_NAME,t.ENGINE as ENGINE,t.DATA_LENGTH/1024/1024/1024 as DATA_LENGTH,t.INDEX_LENGTH/1024/1024/1024 as INDEX_LENGTH,SUM(t.DATA_LENGTH+t.INDEX_LENGTH)/1024/1024/1024 AS TOTAL_LENGTH,c.column_name,c.data_type,c.COLUMN_TYPE,t.AUTO_INCREMENT,locate('unsigned',c.COLUMN_TYPE) = 0 AS IS_SIGNED 
+    FROM information_schema.TABLES t JOIN
+    (
+    SELECT * FROM information_schema.COLUMNS WHERE TABLE_SCHEMA = '{$dbname}' AND extra='auto_increment'
+    ) c ON t.table_name=c.table_name GROUP BY TABLE_NAME ORDER BY TOTAL_LENGTH DESC,AUTO_INCREMENT DESC"; 
 	 
      $result_table_info = mysqli_query($con_table_info,$get_table_info);
 	while($row = mysqli_fetch_array($result_table_info)) 
      {
 		echo "<tr>";
-		echo "<td>{$row['0']}</td>";
-		echo "<td>{$row['1']}</td>";
-		echo "<td>".round($row['2'],3)."</td>";
-  		echo "<td>".round($row['3'],3)."</td>";
-		echo "<td>".round($row['4'],3)."</td>";
+		echo "<td>{$row['TABLE_NAME']}</td>";
+		echo "<td>{$row['ENGINE']}</td>";
+		echo "<td>".round($row['DATA_LENGTH'],3)."</td>";
+  		echo "<td>".round($row['INDEX_LENGTH'],3)."</td>";
+		echo "<td>".round($row['TOTAL_LENGTH'],3)."</td>";
+		echo "<td>{$row['COLUMN_NAME']}</td>";
+		echo "<td>{$row['COLUMN_TYPE']}</td>";
+		echo "<td>{$row['AUTO_INCREMENT']}</td>";
+		
+	      if($row['DATA_TYPE'] == 'int'){
+			if ($row['IS_SIGNED'] == 0){
+				echo "<td>". (4294967295-$row['AUTO_INCREMENT']) ."</td>";
+			}
+			if ($row['IS_SIGNED'] == 1){
+				echo "<td>". (2147483647-$row['AUTO_INCREMENT']) ."</td>";
+			}			
+		}
+	
+	      if($row['DATA_TYPE'] == 'bigint'){
+			if ($row['IS_SIGNED'] == 0){
+				echo "<td>". number_format((18446744073709551615-$row['AUTO_INCREMENT']),0, '', '') ."</td>";
+			}
+			if ($row['IS_SIGNED'] == 1){
+				echo "<td>". (9223372036854775807-$row['AUTO_INCREMENT']) ."</td>";
+			}			
+		}
+		
 	      echo "</tr>";
       }
 	 //end while
